dcdb977aa76929879834557789485d6805993ef3
Jim-Daoulas <daoulas.d.ace@gmail.com>
2025-04-01 12:59:05 +0300

    Update mindtrap.php
    add description

--- mindtrap.php
+++ mindtrap.php
@@ -15,7 +15,7 @@
   <img src="images\mindtrap/1000-and-1-nights.jpg" class="card-img-top" alt="...">
   <div class="card-body">
     <h5 class="card-title">Cast into the Fire</h5>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <p class="card-text">Are you ready for a world-saving adventure?</p>
     <a href="https://aristotelous.themindtrap.com/cast-into-the-fire/?lang=en" class="btn btn-primary">Go somewhere</a>
   </div>
 </div>
